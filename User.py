c2440347abb0968eb96ab89f49ad4e7ab8096a8a
zaryab-ahmad-10 <zaryabahmad2005@gmail.com>
2024-07-31 23:12:30 +0500

    Final changes.

--- User.py
+++ User.py
@@ -1,16 +1,3 @@
 class User:
-    def __init__(self, rental_system: CarRentalSystem, user_name: str):
-        self.rental_system = rental_system
+    def __init__(self, user_name: str):
         self.user_name = user_name
-
-    def view_all_cars(self):
-        self.rental_system.view_all_cars()
-
-    def book_car(self, num_people: int, start_date: datetime, end_date: datetime):
-        available = self.rental_system.cars_available(num_people, start_date, end_date)
-        if available:
-            car = available[0]
-            self.rental_system.book_car(car, start_date, end_date, self.user_name)
-            print(f"Car booked: {car}")
-        else:
-            print("No cars available.")
